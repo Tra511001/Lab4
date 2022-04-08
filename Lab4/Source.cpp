1057245cc98f2d606c45c2acaf492eeb903f8610
NTRAJANOVSKI <89536208+Tra511001@users.noreply.github.com>
2022-04-08 00:55:29 -0500

    Update Source.cpp

--- Lab4/Source.cpp
+++ Lab4/Source.cpp
@@ -6,6 +6,21 @@ using namespace std;
 
 int main() {
 
+	StackType stack1;
+	ItemType item1;
+	ItemType item2;
+	ItemType item3;
+	item1.Initialize(1);
+	item2.Initialize(2);
+	item3.Initialize(3);
 
+	stack1.Push(item1.value);
+	stack1.Push(item2.value);
+	stack1.Push(item3.value);
+	cout << stack1.Top() << endl;
+
+	stack1.reverse();
+	cout << stack1.Top();
+	
 	return 1;
 }
